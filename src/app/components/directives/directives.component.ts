25c89d615acb1f2fb7eed1b6a73b12573f9023f5
Eduardo Toledo <eduardo_toledov@hotmail.com>
2024-05-16 16:05:02 -0600

    Directivas Estructurales

---
+++ src/app/components/directives/directives.component.ts
@@ -0,0 +1,13 @@
+import { NgFor, NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  standalone: true,
+  imports: [NgFor, NgIf, NgSwitchCase, NgSwitch],
+  templateUrl: './directives.component.html',
+  styleUrl: './directives.component.scss'
+})
+export class DirectivesComponent {
+  array: string[] = ['A', 'B', 'C']
+}
